b4f49134d7c9cf3ceb59c7828ea0bc40493056cc
Julia Andrade <juliaaar2009@gmail.com>
2026-09-25 11:35:03 -0300

    Add files via upload

---
+++ aula 21 e 22 funções/main (2).py
@@ -0,0 +1,7 @@
+def apresentar(nome, idade):
+    print("Olá,", nome, "sua idade é", idade, "!")
+apresentar("Adenilson", 78)
+apresentar("João Antônio", 18)
+apresentar("Gustavo Mota", 15)
+
+def somar():
