45dd14bedea73238dcd2073795b4f8ab401c8b15
Julia Andrade <juliaaar2009@gmail.com>
2026-09-25 11:32:48 -0300

    Add files via upload

---
+++ aula 20 dicionário/main (1).py
@@ -0,0 +1,15 @@
+#Lista de dicionários
+
+personagens = [
+    {"Nome": "BOB ESPONJA", "Idade": 23},
+    {"Nome": "THANOS", "Idade": 1000}
+    
+    ]
+    
+# print(personagens [0] ["nome"])
+# print (personagens [1] ["nome"])
+
+for personagem in personagens:
+    print ("Nome:", personagem ["Nome"])
+    print ("Idade:", personagem ["Idade"])
+    print ("--------------")
